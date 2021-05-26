6350a83d73668fdf6fa774ec6c44cf2296e07e38
ujvarivera <ujvariveronika0320@gmail.com>
2021-05-26 13:54:54 +0200

    version 1.7

--- dict_logic.py
+++ dict_logic.py
@@ -7,6 +7,7 @@ class Dictionary:
     """A szótár logikai osztálya."""
     def __init__(self, filename) -> None:
         self.filename = filename
+        self.not_existing_words = [] #Azoknak a szavaknak a listája, amelyeket nem tartalmaz a szotar, de megprobaltad
         if os.path.exists(filename):
             with open(filename,"r") as f:
                 self.data = json.load(f)
@@ -15,17 +16,9 @@ class Dictionary:
         """Visszaad egy random szót a már létező json fájlból."""
         return random.choice(list(self.data.keys()))
 
-    def get_random_definition(self):
-        """Visszaad egy random definíciót a már létező json fájlból."""
-        return random.choice(list(self.data.values()))
-
     def has_word(self,word:str)->bool:
         """Visszaadja, hogy a beírt szó szerepel-e már a szótárban"""
         return word in self.data.keys()
-
-    def is_empty(self, text:str)-> bool: 
-        """Visszaadja, hogy írtunk-e a mezőbe szöveget, vagy sem."""
-        return len(text) == 0
 
     def add_word(self,word:str,definition:str)-> None:
         """Hozzáadja a megadott szót és definíciót a json fájlhoz. 
@@ -33,32 +26,25 @@ class Dictionary:
         if self.has_word(word):
             raise Exception("A megadott szó már szerepel a szótárban!")
 
-        if self.is_empty(word) or self.is_empty(definition):
+        if len(word) == 0 or len(definition) == 0:
             raise Exception("Egyik mező sem maradhat üres!")
 
-        new_word = {word:definition}
-        self.data.update(new_word)
-        with open(self.filename, "w") as f:
-            f.write(json.dumps(self.data))
-        
+        self.data[word] = definition
 
+    def save_words(self):
+        with open(self.filename, "w") as f:
+            f.write(json.dumps(self.data))     
+        
+        
 class Quiz:
     """A QUIZ logikai osztálya."""
     def __init__(self, dictionary:Dictionary)-> None:
-        self.dictionary = dictionary
         self.random_word = None #random kiválasztott szó
         self.the_good_answer = None # a random szó tényleges jelentése
         self.answers = [] # 3 random definíciot tartalmaz, és a jó válaszhoz tartozót 
         self.good = 0 # jó válaszaid számát tárolja, azaz a pontjaid
         self.questions = 0 # feltett kérdések számát tárolja
-
-    def shuffle_elements(self)-> None:
-        """Megkeveri a json elemeit"""
-        data_list = list(self.dictionary.data.items())
-        random.shuffle(data_list)
-        data_dict = dict(data_list)
-        with open(self.dictionary.filename, "w") as f:
-            f.write(json.dumps(data_dict))
+        self.dictionary = dictionary
     
     def result(self):
         """Megmutatja az eredményed az EXIT gomb megnyomása után, hogy hány százalékot értél el."""
@@ -67,18 +53,17 @@ class Quiz:
         except ZeroDivisionError: pass
 
     def make_quiz(self)-> None:
-        """Megkeveri a JSON elemeit, kiválaszt egy random szót, és 4 válaszlehetőséget, melyek közül csak az egyik igaz.
-        Majd ezeket is megkeveri, hogy random sorrendben jelenjenek meg. Ha a json fájl nem tartalmaz legalább 
+        """Kiválaszt egy random szót, és 4 válaszlehetőséget, melyek közül csak az egyik igaz.
+        Ezeket megkeveri, hogy random sorrendben jelenjenek meg. Ha a json fájl nem tartalmaz legalább 
         4 szót, kapunk egy Exceptiont, mivel ismétlődéseket nem szeretnénk látni."""
-        self.shuffle_elements()
         self.random_word = self.dictionary.get_random_word()
         self.the_good_answer = self.dictionary.data[self.random_word]
         self.answers.append(self.the_good_answer)
         if len(self.dictionary.data) < 4: raise Exception("Dict must have at least 4 elements before starting QUIZ.")
         while len(self.answers) != 4:
-            new = self.dictionary.get_random_definition()
-            if new not in self.answers:
-                self.answers.append(new)
+            word = self.dictionary.get_random_word()
+            if self.dictionary.data[word] not in self.answers:
+                self.answers.append(self.dictionary.data[word])
         random.shuffle(self.answers)
 
 
@@ -95,20 +80,17 @@ class Plot:
 
     def make_plot(self)-> None:
         """Csinál egy plotot."""
-        plt.cla()
-        plt.title("Your answers")
         x = ["bad answers", "good answers", "number of questions"]
         y = [(self.quiz.questions-self.quiz.good), self.quiz.good, self.quiz.questions]
-        plt.bar(x,y)
-        plt.show()
 
+        fig, ax = plt.subplots()
+        ax.bar(x, y)
+        ax.set_title('Your answers')
+        fig.show()
 
 
 if __name__=="__main__":
     dictionary = Dictionary("dictionary_of_words.json")
-
     print("\nA random word: ",dictionary.get_random_word())
-    print("A random definition: ",dictionary.get_random_definition())
-
     dictionary.add_word("foo","bar")
     
